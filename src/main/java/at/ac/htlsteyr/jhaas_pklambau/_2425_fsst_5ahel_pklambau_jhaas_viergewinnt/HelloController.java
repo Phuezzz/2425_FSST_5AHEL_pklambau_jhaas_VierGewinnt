9223e73b763bad779850eed0e30e952bfd8dd5f6
Phuezzz <pklambau@htl-steyr.ac.at>
2024-12-04 08:43:28 +0100

    JavaFX conversion

---
+++ src/main/java/at/ac/htlsteyr/jhaas_pklambau/_2425_fsst_5ahel_pklambau_jhaas_viergewinnt/HelloController.java
@@ -0,0 +1,14 @@
+package at.ac.htlsteyr.jhaas_pklambau._2425_fsst_5ahel_pklambau_jhaas_viergewinnt;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module at.ac.htlsteyr.jhaas_pklambau._2425_fsst_5ahel_pklambau_jhaas_viergewinnt {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens at.ac.htlsteyr.jhaas_pklambau._2425_fsst_5ahel_pklambau_jhaas_viergewinnt to javafx.fxml;
+    exports at.ac.htlsteyr.jhaas_pklambau._2425_fsst_5ahel_pklambau_jhaas_viergewinnt;
+}
